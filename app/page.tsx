998f92512977e9ba71eb0a8e73713f16eac0529f
Kevin Sanchez <Chilledsonic.ks2@gmail.com>
2023-09-22 16:27:54 -0400

    Added Clerk for Sign in, Sign out Functionality

--- app/page.tsx
+++
@@ -1,9 +0,0 @@
-import { Button } from "@/components/ui/button"
-
-export default function Home() {
-  return (
-    <div className="flex flex-col">
-      <p className='text-3xl font-bold text-indigo-600'>Hello Discord Clone</p>
-    </div>
-  )
-}
